248de18f84389bc7d4d25eeb45386c44c4b8e6a3
kronos <guilhermegomes7770@gmail.com>
2025-05-14 20:04:55 -0300

    b

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -6,15 +6,15 @@ import { showPost } from "../services/post.services";
 const routes = new Elysia({ prefix: "/post" })
   .post("/create", ({ body, set }) => createPost(body, set), {
     body: t.Object({
-      title: t.String({ description: "Post title" }),
-      author: t.String({ description: "Post author" }),
-      text: t.String({ description: "Post text | content" }),
+      title: t.String({ description: "Post Title" }),
+      author: t.String({ description: "Post Author" }),
+      text: t.String({ description: "Post Text | Content" }),
     }),
   })
   .get("/", allPost())
   .get("/show/:slug", ({ params: { slug }, set }) => showPost(slug, set), {
     params: t.Object({
-      slug: t.String({ description: "Post UUID" }),
+      slug: t.String({ description: "Post Slug" }),
     }),
   });
 
